277c2d5eb742a7e9b40bc95ce7eaf1c184a5e466
IAmJaysWay <91806278+IAmJaysWay@users.noreply.github.com>
2023-09-18 20:35:44 +0300

    Add files via upload

---
+++ EfficientFrontier.R
@@ -0,0 +1,32 @@
+#### Start Of Efficient Frontier ####
+
+# Bring in dependencies
+library("httr")
+library("jsonlite")
+library("keyring")
+library("dplyr")
+library("ggplot2")
+library("plotly")
+
+# Get & Format Price History Data
+
+
+# Get Daily Returns and Summary Data
+
+
+# Calculate X an Variance-Covariance Matrices
+
+
+# Expected Return and Volatility For Equally weighted portfolio
+
+
+# Simulate Multiple Portfolio Weights
+
+
+# Expected Return and Volatility For Different Weighted portfolio
+
+
+# Generate Interactive Efficeint Frontier Chart
+
+
+#### End Of Efficient Frontier ####
